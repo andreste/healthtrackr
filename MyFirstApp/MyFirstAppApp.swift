c20449fe297bc0543e4aeb72b4f5089f88335fb2
Andres Trevino <andreste24@gmail.com>
2026-03-21 18:02:26 -0400

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Andres Trevino on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
